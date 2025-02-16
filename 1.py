f6e62507745d02bc61ffc7bf889b9b5d3012e9d4
AHhMOo <168608113+AHhMOo@users.noreply.github.com>
2025-02-16 11:59:32 -0500

    1.py

--- 1.py
+++ 1.py
@@ -1,1 +1,1 @@
-#i have a problem with vint and i have to move on very fast cuz i dont have much time and closer 1v1 its monday
+#i have a problem with vite and i have to move on very fast cuz i dont have much time and closer 1v1 its monday
